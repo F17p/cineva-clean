437315deadd0fb0d96dbb3911ff2c7ba34f813bb
F17p <felicianondala95@gmail.com>
2025-08-27 20:42:45 +0100

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+// pages/about.js
+export default function About() {
+  return (
+    <div className="container">
+      <h1>Sobre o Cineva</h1>
+      <p style={{color:"#9ca3af"}}>Projeto de exemplo para testar catálogo e player local (uso pessoal).</p>
+    </div>
+  )
+}
