b0290a7011ff27204de123241021db9f29a0f09d
F17p <felicianondala95@gmail.com>
2025-08-27 20:45:10 +0100

    Create contact.js

---
+++ pages/contact.js
@@ -0,0 +1,10 @@
+// pages/contact.js
+export default function Contact() {
+  return (
+    <div className="container">
+      <h1>Contato</h1>
+      <p>Email: <a href="mailto:cineva017@gmail.com">cineva017@gmail.com</a></p>
+      <p>WhatsApp: <a href="https://wa.me/244952356080" target="_blank">+244 952 356 080</a></p>
+    </div>
+  )
+}
